d7e0a3457e703c4f0758e7f02bfc867453d40874
Ilán Vivanco <ilanvivanco@gmail.com>
2022-09-05 22:41:28 +0200

    Add basic structure

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<Timeline :timelineData="people" />
+</template>
+
+<script>
+import Timeline from './components/Timeline.vue'
+import people from './data/people'
+
+export default {
+  name: 'App',
+  data:()=>({
+    people
+  }),
+  components: {
+    Timeline
+  },
+}
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
